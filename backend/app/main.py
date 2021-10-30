6afa1251c25281d45a22c251180ae340f68cc69f
Gary Tyler <mail@garytyler.com>
2021-10-30 14:40:44 -0400

    Add scaffolding for python backend

---
+++ backend/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, FastAPI
+
+api_router = APIRouter()
+
+
+@api_router.get("/")
+async def root():
+    return {"message": "Hello World!"}
+
+
+def get_app():
+    app = FastAPI()
+    app.include_router(api_router)
+    return app
+
+
+app = get_app()
