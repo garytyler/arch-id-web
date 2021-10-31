c6b14e6d844fe0d16e9db41c269f035078975036
Gary Tyler <mail@garytyler.com>
2021-10-31 23:06:58 +0000

    Add simple file selector

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,44 @@
-import React from "react";
+import { ChangeEvent, useState } from "react";
 import "./App.css";
-import logo from "./logo.svg";
+
+function Main() {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const changeHandler = (event: ChangeEvent<HTMLInputElement> | null) => {
+    if (event?.target?.files) {
+      setSelectedFile(event.target.files[0]);
+    }
+  };
+
+  const input = () => {
+    return <input type="file" onChange={changeHandler} />;
+  };
+
+  if (selectedFile) {
+    return (
+      <div>
+        {input()}
+        <br />
+        <h2>File Details:</h2>
+        <p>File Name: {selectedFile.name}</p>
+        <p>File Type: {selectedFile.type}</p>
+        <p>FileSize: {selectedFile.size}</p>
+        <p>Last Modified: {selectedFile.lastModified.toLocaleString()}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {input()}
+        <br />
+        <h4>Choose before Pressing the Upload button</h4>
+      </div>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">{Main()}</div>;
 }
 
 export default App;
