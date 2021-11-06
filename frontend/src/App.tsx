19dec8b035a98ba1034e605e23355177949dbda5
Gary Tyler <mail@garytyler.com>
2021-11-06 19:24:14 +0000

    Get image data from selected file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,24 @@ import "./App.css";
 function Main() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+
   const changeHandler = (event: ChangeEvent<HTMLInputElement> | null) => {
     if (event?.target?.files) {
       setSelectedFile(event.target.files[0]);
+
+      const image = new Image(299, 299);
+      image.src = URL.createObjectURL(event.target.files[0]);
+      image.onload = () => {
+        if (context) {
+          context.drawImage(image, 0, 0);
+          const imageData = context.getImageData(0, 0, 299, 299);
+          // document.getElementById("placehere")?.appendChild(image);
+          document.body.appendChild(image);
+          console.log(imageData);
+        }
+      };
     }
   };
 
@@ -24,6 +39,7 @@ function Main() {
         <p>File Type: {selectedFile.type}</p>
         <p>FileSize: {selectedFile.size}</p>
         <p>Last Modified: {selectedFile.lastModified.toLocaleString()}</p>
+        <img alt="Source" id="placeholder"></img>
       </div>
     );
   } else {
