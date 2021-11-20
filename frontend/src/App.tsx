671f8a0d9786a2dec5f705c9c570efeb9dfedb81
Gary Tyler <mail@garytyler.com>
2021-11-20 08:51:39 +0000

    Untrack old logo files, update corner logo.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -80,7 +80,7 @@ export default function App() {
                     color="secondary"
                     noWrap
                     component="span"
-                    style={{ fontWeight: 900 }}
+                    style={{ fontStyle: "italic" }}
                   >
                     ID
                   </Typography>
